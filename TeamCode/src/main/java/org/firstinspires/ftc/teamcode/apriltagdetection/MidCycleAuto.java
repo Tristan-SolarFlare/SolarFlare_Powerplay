55ff0d6aee4407def7b7227b2f9c04b9e9da24d5
Tristan-SolarFlare <tristan@solarflarerobotics.org>
2024-06-22 17:05:38 -0400

    Update MidCycleAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/MidCycleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/MidCycleAuto.java
@@ -69,41 +69,51 @@ public class MidCycleAuto extends LinearOpMode
     AprilTagDetection tagOfInterest = null;
 
     public class Lift {
-        private DcMotorEx slide1;
-        private DcMotorEx slide2;
-
-        public Lift(HardwareMap hardwareMap) {
-            DcMotorEx slide1 = hardwareMap.get(DcMotorEx.class,"slide1");
-            DcMotorEx slide2 = hardwareMap.get(DcMotorEx.class,"slide2");
-
-            slide1.setDirection(DcMotorSimple.Direction.REVERSE);
-            slide2.setDirection(DcMotorSimple.Direction.FORWARD);
-
-            // Ensures that when no power is set on the motors they will hold their position
-            slide1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            slide2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-            slide1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            slide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-            slide1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            slide2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        }
         // mid=120
         // topcone=150
         // second=120
         // third=85
         // fourth=40
         // last=0
-
         public class TopCone implements Action{
             @Override
             public boolean run(@NonNull TelemetryPacket packet){
+                DcMotorEx slide1 = hardwareMap.get(DcMotorEx.class,"slide1");
+                DcMotorEx slide2 = hardwareMap.get(DcMotorEx.class,"slide2");
+
+                slide1.setDirection(DcMotorSimple.Direction.REVERSE);
+                slide2.setDirection(DcMotorSimple.Direction.FORWARD);
+
+                // Ensures that when no power is set on the motors they will hold their position
+                slide1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                slide2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+                slide1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                slide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+                slide1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                slide2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+                Servo arm1 = hardwareMap.servo.get("arm");
+                Servo arm2 = hardwareMap.servo.get("arm2");
+
+                arm1.setDirection(Servo.Direction.REVERSE);
+
+
+
+                Servo claw = hardwareMap.servo.get("claw");
+
+                Servo wrist = hardwareMap.servo.get("wrist");
                 double Kp=0.015;
-                while (slide1.getCurrentPosition() <150 && slide2.getCurrentPosition() <150){
+                arm1.setPosition(0.02);
+                arm2.setPosition(0.02);
+                wrist.setPosition(0.91);
+                claw.setPosition(0);
+                sleep(300);
+                while (slide1.getCurrentPosition() <145 && slide2.getCurrentPosition() <145){
                     double slide1power;
                     // Calculates amount of ticks off slide1 is from target
-                    double error1=-(175-slide1.getCurrentPosition()); // Error is negative because slide1 needs to reverse direction
+                    double error1=-(170-slide1.getCurrentPosition()); // Error is negative because slide1 needs to reverse direction
 
                     // If error1 is greater than 80, correct by faster speed, else correct by usual speed
                     if ((Math.abs(error1)>80)){
@@ -118,7 +128,7 @@ public class MidCycleAuto extends LinearOpMode
                     double slide2power;
 
                     // Calculates amount of ticks off slide2 is from target
-                    double error2=175-slide2.getCurrentPosition();
+                    double error2=170-slide2.getCurrentPosition();
 
                     // If error1 is greater than 80, correct by faster speed, else correct by usual speed
                     if ((Math.abs(error2)>80)){
@@ -129,11 +139,48 @@ public class MidCycleAuto extends LinearOpMode
                     }
                     slide2.setPower(slide2power);
                 }
+                claw.setPosition(0.3);
+                sleep(500);
+                arm1.setPosition(0.96);
+                arm2.setPosition(0.96);
 
-                return true;
+                sleep(1000);
+                while (slide1.getCurrentPosition() >120 && slide2.getCurrentPosition() >120){
+
+                    double slide1power;
+                    // Calculates amount of ticks off slide1 is from target
+                    double error1=-(100-slide1.getCurrentPosition()); // Error is negative because slide1 needs to reverse direction
+
+                    // If error1 is greater than 80, correct by faster speed, else correct by usual speed
+                    if ((Math.abs(error1)>80)){
+                        slide1power = (0.75*error1);
+                    }
+                    else {
+                        slide1power = (Kp*error1);
+                    }
+                    slide1.setPower(slide1power);
+
+                    // We need a new slide2 power that will correct for error
+                    double slide2power;
+
+                    // Calculates amount of ticks off slide2 is from target
+                    double error2=100-slide2.getCurrentPosition();
+
+                    // If error1 is greater than 80, correct by faster speed, else correct by usual speed
+                    if ((Math.abs(error2)>80)){
+                        slide2power = 0.75*error2;
+                    }
+                    else {
+                        slide2power = Kp*error2;
+                    }
+                    slide2.setPower(slide2power);
+                }
+                return false;
             }
         }
-
+        public Action topCone(){
+            return new TopCone();
+        }
     }
 
     @Override
@@ -432,9 +479,11 @@ public class MidCycleAuto extends LinearOpMode
         else if (tagOfInterest.id == rightTag){
             trajectoryActionChosen = parkingZone3;
         }
+        Lift lift= new Lift();
         Actions.runBlocking(
                 new SequentialAction(
-                        trajectoryActionChosen
+                        lift.topCone()
+
                 )
         );
 
