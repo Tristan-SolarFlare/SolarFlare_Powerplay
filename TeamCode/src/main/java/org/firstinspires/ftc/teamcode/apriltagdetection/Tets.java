0fd8a558c99cf7f46910660a452a17357a214ab9
DaniNayal <dani@nayal.org>
2024-08-02 13:34:07 -0400

    e

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/Tets.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/Tets.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.apriltagdetection;
 
+import static java.lang.Thread.sleep;
+
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
@@ -12,6 +14,7 @@ import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -27,7 +30,7 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 import java.util.ArrayList;
 
 @Autonomous
-public class Test extends LinearOpMode {
+public class Tets extends OpMode {
     OpenCvCamera camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
     static final double FEET_PER_METER = 3.28084;
@@ -47,6 +50,15 @@ public class Test extends LinearOpMode {
     double arm2pos = 0.02;
     double clawpos = 0.3;
     double wristpos = 0.91;
+    private boolean ran = false;
+    public Lift lift;
+    public boolean tagFound = false;
+    public Action DriveInitialDeposit;
+    public Action ParkZone1;
+    public Action ParkZone2;
+    public Action ParkZone3;
+    public Action parkingZone;
+
     public class Lift {
         DcMotorEx slide1 = hardwareMap.get(DcMotorEx.class, "slide1");
         DcMotorEx slide2 = hardwareMap.get(DcMotorEx.class, "slide2");
@@ -55,53 +67,52 @@ public class Test extends LinearOpMode {
         Servo wrist = hardwareMap.servo.get("wrist");
         Servo claw = hardwareMap.servo.get("claw");
 
-        public class SetSlidesTarget implements Action{
-            public boolean run(@NonNull TelemetryPacket telemetryPacket){
-                telemetry.addLine("SetSlidesTarget.run(" + target + ")");
+        public class SetSlidesTarget implements Action {
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
                 return false;
             }
-            public SetSlidesTarget(int y){
-                target = y;
+
+            public SetSlidesTarget(int e) {
+                target = e;
             }
         }
 
-        public class OpenClaw implements Action{
-            public boolean run(@NonNull TelemetryPacket telemetryPacket){
+        public class OpenClaw implements Action {
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
                 clawpos = 0;
-                telemetry.addLine("open claw ran");
                 return false;
             }
         }
-        public class ArmsDown implements Action{
-            public boolean run(@NonNull TelemetryPacket telemetryPacket){
+
+        public class ArmsDown implements Action {
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
                 arm1pos = 0.02;
                 arm2pos = 0.02;
                 wristpos = 0.91;
                 clawpos = 0.3;
-                telemetry.addLine("arms down ran");
-                return false;
-            }
-        }
-        public class CloseClaw implements Action{
-            public boolean run(@NonNull TelemetryPacket telemetryPacket){
-                clawpos = 0.3;
-                telemetry.addLine("close claw ran");
-                return false;
-            }
-        }
-        public class ArmsUp implements Action{
-            public boolean run(@NonNull TelemetryPacket telemetryPacket){
-                arm1pos = 0.96;
-                arm2pos = 0.96;
-                wristpos = 0.3;
-                clawpos = 0.3;
-                telemetry.addLine("arms up ran");
                 return false;
             }
         }
 
-        public class Init implements Action{
-            public boolean run(@NonNull TelemetryPacket telemetryPacket){
+        public class CloseClaw implements Action {
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                clawpos = 0.3;
+                return false;
+            }
+        }
+
+        public class ArmsUp implements Action {
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                arm1pos = 0.96;
+                arm2pos = 0.96;
+                wristpos = 0.3;
+                clawpos = 0.3;
+                return false;
+            }
+        }
+
+        public class Init implements Action {
+            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
                 arm1.setDirection(Servo.Direction.REVERSE);
                 arm2.setDirection(Servo.Direction.FORWARD);
 
@@ -118,61 +129,74 @@ public class Test extends LinearOpMode {
                 arm2.setPosition(0.02);
                 wrist.setPosition(0.91);
                 claw.setPosition(0.3);
-                telemetry.addLine("init ran");
+
                 return false;
             }
         }
 
-        public class GlobalPID implements Action {
-            public boolean run(@NonNull TelemetryPacket telemetryPacket){
+        public void GlobalPID() {
 
-                    int error1 = target - slide1.getCurrentPosition();
-                    slide1.setPower(-error1 * Kp);
-                    int error2 = target - slide2.getCurrentPosition();
-                    slide2.setPower(error2 * Kp);
+            int error1 = target - slide1.getCurrentPosition();
+            slide1.setPower(-error1 * Kp);
+            int error2 = target - slide2.getCurrentPosition();
+            slide2.setPower(error2 * Kp);
 
-                    arm1.setPosition(arm1pos);
-                    arm2.setPosition(arm2pos);
-                    claw.setPosition(clawpos);
-                    wrist.setPosition(wristpos);
+            arm1.setPosition(arm1pos);
+            arm2.setPosition(arm2pos);
+            claw.setPosition(clawpos);
+            wrist.setPosition(wristpos);
 
-                telemetry.addData("slide1 pos", slide1.getCurrentPosition());
-                telemetry.addData("slide2 pos", slide2.getCurrentPosition());
-                telemetry.addData("target",target);
-                telemetry.addData("iterations",iterations++);
-                telemetry.update();
-
-                return true;
-            }
+            telemetry.addData("slide1 pos", slide1.getCurrentPosition());
+            telemetry.addData("slide2 pos", slide2.getCurrentPosition());
+            telemetry.addData("iterations", iterations++);
+            telemetry.update();
         }
 
-        public Action globalPID() {return new GlobalPID();}
-        public Action initialize(){return new Init();}
-        public Action openClaw(){return new OpenClaw();}
-        public Action closeClaw(){return new CloseClaw();}
-        public Action setSlidesTarget(int y){return new SetSlidesTarget(y);}
-        public Action armsUp(){return new ArmsUp();}
-        public Action armsDown(){return new ArmsDown();}
+        public Action initialize() {
+            return new Init();
+        }
 
+        public Action setSlidesTarget(int e) {
+            return new SetSlidesTarget(e);
+        }
+
+        public Action openClaw() {
+            return new OpenClaw();
+        }
+
+        public Action armsDown() {
+            return new ArmsDown();
+        }
+
+        public Action closeClaw() {
+            return new CloseClaw();
+        }
+
+        public Action armsUp() {
+            return new ArmsUp();
+        }
+
+        public void globalPidUpdate() {
+            GlobalPID();
+        }
     }
 
-    public void runOpMode() {
+
+    @Override
+    public void init() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
 
         camera.setPipeline(aprilTagDetectionPipeline);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
-        {
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
-            public void onOpened()
-            {
-                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            public void onOpened() {
+                camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
-            public void onError(int errorCode)
-            {
+            public void onError(int errorCode) {
 
             }
         });
@@ -181,164 +205,130 @@ public class Test extends LinearOpMode {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11, 36, Math.toRadians(0)));
 
         Action DriveInitialDeposit = drive.actionBuilder(drive.pose)
-                .strafeTo(new Vector2d(11,72))
-                .strafeTo(new Vector2d(36,72))
+                .strafeTo(new Vector2d(11, 72))
+                .strafeTo(new Vector2d(36, 72))
                 .build();
 
         Action ParkZone1 = drive.actionBuilder(drive.pose)
-                .strafeTo(new Vector2d(11,72))
-                .strafeTo(new Vector2d(11,60))
-                .strafeTo(new Vector2d(36,60))
+                .strafeTo(new Vector2d(11, 72))
+                .strafeTo(new Vector2d(11, 60))
+                .strafeTo(new Vector2d(36, 60))
                 .build();
 
         Action ParkZone2 = drive.actionBuilder(drive.pose)
-                .strafeTo(new Vector2d(11,72))
-                .strafeTo(new Vector2d(11,36))
-                .strafeTo(new Vector2d(36,36))
+                .strafeTo(new Vector2d(11, 72))
+                .strafeTo(new Vector2d(11, 36))
+                .strafeTo(new Vector2d(36, 36))
                 .build();
 
         Action ParkZone3 = drive.actionBuilder(drive.pose)
-                .strafeTo(new Vector2d(11,72))
-                .strafeTo(new Vector2d(11,12))
-                .strafeTo(new Vector2d(36,12))
+                .strafeTo(new Vector2d(11, 72))
+                .strafeTo(new Vector2d(11, 12))
+                .strafeTo(new Vector2d(36, 12))
                 .build();
 
-        boolean tagFound = false;
+        tagFound = false;
 
-        waitForStart();
+        lift = new Lift();
+        Actions.runBlocking(lift.initialize());
+    }
 
-        if (isStopRequested()) return;
+    @Override
+    public void start() {
 
-        while (!tagFound)
-        {
+        while (!tagFound) {
             ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
 
-            if(currentDetections.size() != 0)
-            {
+            if (currentDetections.size() != 0) {
                 tagFound = false;
 
-                for(AprilTagDetection tag : currentDetections)
-                {
-                    if(tag.id == leftTag || tag.id == middleTag || tag.id == rightTag)
-                    {
+                for (AprilTagDetection tag : currentDetections) {
+                    if (tag.id == leftTag || tag.id == middleTag || tag.id == rightTag) {
                         tagOfInterest = tag;
                         tagFound = true;
                         break;
                     }
                 }
 
-                if(tagFound)
-                {
+                if (tagFound) {
                     telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
-                }
-                else
-                {
+                } else {
                     telemetry.addLine("Don't see tag of interest :(");
 
-                    if(tagOfInterest == null)
-                    {
+                    if (tagOfInterest == null) {
                         telemetry.addLine("(The tag has never been seen)");
-                    }
-                    else
-                    {
+                    } else {
                         telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
                     }
                 }
 
-            }
-            else
-            {
+            } else {
                 telemetry.addLine("Don't see tag of interest :(");
 
-                if(tagOfInterest == null)
-                {
+                if (tagOfInterest == null) {
                     telemetry.addLine("(The tag has never been seen)");
-                }
-                else
-                {
+                } else {
                     telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
                 }
 
             }
 
             telemetry.update();
-            sleep(20);
         }
 
 
         /* Update the telemetry */
-        if(tagOfInterest != null)
-        {
+        if (tagOfInterest != null) {
             telemetry.addLine("Tag snapshot:\n");
             telemetry.update();
-        }
-        else
-        {
+        } else {
             telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
             telemetry.update();
         }
 
-        Action parkingZone = null;
+        parkingZone = null;
 
-        if(tagOfInterest == null){
+        if (tagOfInterest == null) {
 
             // Runs autonomous and parks in zone 2
             // Runs if camera does not detect april tag
             parkingZone = ParkZone2;
         }
-        if(tagOfInterest.id == leftTag){
+        if (tagOfInterest.id == leftTag) {
 
             parkingZone = ParkZone1;
-        }
-        else if (tagOfInterest.id == middleTag){
+        } else if (tagOfInterest.id == middleTag) {
 
             parkingZone = ParkZone2;
-        }
-        else if (tagOfInterest.id == rightTag){
+        } else if (tagOfInterest.id == rightTag) {
 
             parkingZone = ParkZone3;
         }
-        Lift lift = new Lift();
-        Actions.runBlocking(new SequentialAction(
-                lift.initialize(),
-                new ParallelAction(
-                        lift.globalPID(),
-                        new SequentialAction(
-                                /*
+
+
+    }
+
+
+    @Override
+    public void loop() {
+        lift.globalPidUpdate();
+        if (!ran) {
+            ran = true;
+            Actions.runBlocking(
+                    new SequentialAction(
                             DriveInitialDeposit,
-                                new SleepAction(2),
-                                lift.setSlidesTarget(440),
-                                lift.armsUp(),
-                                new SleepAction(2),
-                                lift.openClaw(),
-                                new SleepAction(2),
-                                lift.armsDown(),
-                                new SleepAction(0.75),
-                                lift.setSlidesTarget(0),
-                                parkingZone
-
-                                 */
-                                lift.setSlidesTarget(440),
-                                new SleepAction(5000),
-                                lift.setSlidesTarget(300)
-
-
-
-
-                        )
-                )
-                )
-
-        );
-
-        while (opModeIsActive()) {
-            sleep(20);
+                            new SleepAction(2),
+                            lift.setSlidesTarget(440),
+                            lift.armsUp(),
+                            new SleepAction(2),
+                            lift.openClaw(),
+                            new SleepAction(2),
+                            lift.armsDown(),
+                            new SleepAction(0.75),
+                            lift.setSlidesTarget(0),
+                            parkingZone
+                    )
+            );
         }
     }
 }
-
-
-
-
-
-
