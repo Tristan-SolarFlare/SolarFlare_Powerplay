d4676d1fe01311c5c005d5a5627d2558a8e580a3
roobootics <arick@solarflarerobotics.org>
2024-08-03 17:09:25 -0400

    change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/ObjectDetectionCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/ObjectDetectionCV.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode.apriltagdetection;
+
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
@@ -20,21 +21,51 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-
+import org.openftc.easyopencv.OpenCvPipeline;
 
 import java.util.ArrayList;
 @Autonomous
 public class ObjectDetectionCV extends LinearOpMode{
+
+    OpenCvCamera camera;
     public void runOpMode(){
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
+        camera.setPipeline(new DetectionPipeline());
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode){
+
+            }
+        });
     }
-    /*
+
     public class DetectionPipeline extends OpenCvPipeline{
+        public Mat processFrame(Mat input){
 
+            Imgproc.cvtColor(input,input,Imgproc.COLOR_RGB2YCrCb);
+            Rect left = new Rect(1,1,267,400);
+            Rect middle = new Rect(267,1,266,400);
+            Rect right = new Rect(533,1,267,400);
+
+            return input;
+        }
     }
-     */
+
 }
