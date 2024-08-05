dbdbccf08b46de2f58af78c5f4d1e74fe7734c98
roobootics <arick@solarflarerobotics.org>
2024-08-05 10:15:34 -0400

    change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/ObjectDetectionCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/ObjectDetectionCV.java
@@ -25,7 +25,7 @@ public class ObjectDetectionCV extends LinearOpMode{
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
 
-        camera.setPipeline(new DetectionPipeline2());
+        camera.setPipeline(new DetectionPipeline());
 
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
         {
@@ -41,49 +41,7 @@ public class ObjectDetectionCV extends LinearOpMode{
             }
         });
     }
-
-    public class DetectionPipeline1 extends OpenCvPipeline{
-        public Mat processFrame(Mat input){
-
-            Imgproc.cvtColor(input,input,Imgproc.COLOR_RGB2YCrCb);
-            Rect left = new Rect(1,1,370,400);
-            Rect middle = new Rect(370,1,60,400);
-            Rect right = new Rect(430,1,370,400);
-
-            Mat leftCrop=input.submat(left);
-            Mat middleCrop=input.submat(middle);
-            Mat rightCrop=input.submat(right);
-
-            Core.extractChannel(leftCrop, leftCrop, 3);
-            Core.extractChannel(middleCrop, middleCrop, 3);
-            Core.extractChannel(rightCrop, rightCrop, 3);
-
-            double leftavg = Core.mean(leftCrop).val[0];
-            double midavg = Core.mean(middleCrop).val[0];
-            double rightavg = Core.mean(rightCrop).val[0];
-
-            if (midavg>leftavg && midavg>rightavg){
-                location=2;
-            }
-            else if(leftavg>midavg && leftavg>rightavg){
-                location=1;
-            }
-            else if(rightavg>midavg && rightavg>leftavg){
-                location=3;
-            }
-            else{
-                location=0;
-            }
-            //bro ngl this one's kinda weird if the cone is not guaranteed to be in view
-            Core.extractChannel(input,input,3);
-            telemetry.addData("Location",location);
-            telemetry.addData("left",leftavg);
-            telemetry.addData("mid",midavg);
-            telemetry.addData("right",rightavg);
-            return input;
-        }
-    }
-    public class DetectionPipeline2 extends OpenCvPipeline{
+    public class DetectionPipeline extends OpenCvPipeline{
 
         public Scalar lower = new Scalar(83,107,184);
         public Scalar upper = new Scalar(108,110,176);
