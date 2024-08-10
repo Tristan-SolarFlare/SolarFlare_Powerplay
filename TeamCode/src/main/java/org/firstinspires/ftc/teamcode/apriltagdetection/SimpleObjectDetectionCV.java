7c4b2051c239bbb6b178bac036b964330685f2c5
roobootics <arick@solarflarerobotics.org>
2024-08-10 16:56:41 -0400

    change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/SimpleObjectDetectionCV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/apriltagdetection/SimpleObjectDetectionCV.java
@@ -106,8 +106,8 @@ public class SimpleObjectDetectionCV extends LinearOpMode{
     }
     public class DetectionPipeline2 extends OpenCvPipeline{
 
-        public Scalar lower = new Scalar(0,85,201);
-        public Scalar upper = new Scalar(50,155,255);
+        public Scalar lower = new Scalar(0,0,100);
+        public Scalar upper = new Scalar(50,175,255);
         public Mat processFrame(Mat input){
 
             Rect left = new Rect(1,1,267,400);
